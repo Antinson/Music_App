102e5e825497f49f0888b77c019e2536e0f8b468
Ant279a <amit279@aucklanduni.ac.nz>
2022-08-31 00:33:47 +1200

    Added templates and browse_tracks.py

--- music/__init__.py
+++ music/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
 from music.domainmodel.track import Track
+from music.tracks.tracks_browse import tracks_browse
 
 
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
@@ -18,11 +19,6 @@ def create_some_track():
 
 def create_app():
     app = Flask(__name__)
-
-    @app.route('/')
-    def home():
-        some_track = create_some_track()
-        # Use Jinja to customize a predefined html page rendering the layout for showing a single track.
-        return render_template('simple_track.html', track=some_track)
+    app.register_blueprint(tracks_browse)
 
     return app
--- music/tracks/tracks_browse.py
+++ music/tracks/tracks_browse.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, render_template
 
-tracks_browse = Blueprint('tracks_browse', __name__, static_folder='static', template_folder='templates', url_prefix='/tracks')
+# Setting up our blueprint
+tracks_browse = Blueprint('tracks_browse', __name__, template_folder='templates')
 
-headings = ("Track_ID", "Track_Name", "Artist_Name","Track_Length")
 
 @tracks_browse.route("/browse")
-
 def table():
-    return render_template('tracks_browse.html')
-
-
+    header = ["Track Id", "Track Name", "Artist", "Length"]
+    # Delete when we know how to get the data
+    data = ["1", "Track 1", "Artist 1", "Length 1"]
+    #return '<h1>Hey</h1>'
+    return render_template('browse_tracks.html', headings = header, data = data)
