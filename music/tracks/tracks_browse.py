5a96e3c9203042dcec4836ae8bd3dd2c84986f60
Ant279a <110330425+Ant279a@users.noreply.github.com>
2022-08-31 15:39:56 +1200

    Few updates
    Few updates

--- music/tracks/tracks_browse.py
+++ music/tracks/tracks_browse.py
@@ -7,7 +7,6 @@ tracks_browse = Blueprint('tracks_browse', __name__, template_folder='templates'
 @tracks_browse.route("/browse")
 def table():
     header = ["Track Id", "Track Name", "Artist", "Length"]
-    # Delete when we know how to get the data
-    data = ["1", "Track 1", "Artist 1", "Length 1"]
-    #return '<h1>Hey</h1>'
+
+    # data should be in the form of a list of tracks
     return render_template('browse_tracks.html', headings = header, data = data)
