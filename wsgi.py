3c60995c20abcbaa4a76710c2be5ad03df2224bc
Ant279a <110330425+Ant279a@users.noreply.github.com>
2022-09-05 16:56:08 +1200

    Updated visuals

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from music import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5001, threaded=False)
+    app.run(host='localhost', port=5001, threaded=False, debug=True)
