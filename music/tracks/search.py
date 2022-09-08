ac570c66101ce49674bd393422f69f26362f98b3
Ant279a <110330425+Ant279a@users.noreply.github.com>
2022-09-08 19:25:50 +1200

    New search
    Condensed Search onto one html document and no problems with "/" in title

--- music/tracks/search.py
+++ music/tracks/search.py
@@ -2,13 +2,115 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 import music.tracks.services as services
 import music.adapters.repository as repo
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Length, ValidationError
+
 
 
 search_blueprint = Blueprint('search_bp', __name__, template_folder='templates')
 
+@search_blueprint.route('/search', methods=['GET', 'POST'])
+def search():
+    search_form = SearchForm()
+    if request.method == 'POST':
+        return search_by(search_form)
+
+    # For a GET request, return the search page.
+    return render_template(
+        'tracks/search.html',
+        title='Search',
+        form=search_form,
+        search_target=url_for('search_bp.search')
+    )
+
+def search_by(form):
+    if (form.search_type.data == 'track'):
+        return search_by_track(form.search.data)
+    elif (form.search_type.data == 'genre'):
+        return search_by_genre(form.search.data)
+    elif (form.search_type.data == 'artist'):
+        return search_by_artist(form.search.data)
+    elif (form.search_type.data == 'date'):
+        return search_by_date(form.search.data)
+    elif (form.search_type.data == 'album'):
+        return search_by_album(form.search.data)
+
+def search_by_album(target_album):
+    header = ["Album Id", "Title"]
+    category = "album"
+    # Search for the album.
+
+    tracks_by_album = services.get_tracks_by_album(target_album, repo.repo_instance)
+    if len(tracks_by_album) == 0:
+        return redirect(url_for('search_bp.not_found'))
+
+    # number of tracks found by target_album
+    table_name = str(len(tracks_by_album)) + " results for " + target_album
+
+    return render_template('tracks/browse_tracks_by_category.html', headings=header, table_name=table_name, tracks=tracks_by_album, category=category)
+
+def search_by_artist(target_artist):
+    header = ["Artist Id", "Name"]
+    category = "artist"
+    # Search for the artist.
+
+    tracks_by_artist = services.get_tracks_by_artist(target_artist, repo.repo_instance)
+    if len(tracks_by_artist) == 0:
+        return redirect(url_for('search_bp.not_found'))
+
+    # number of tracks found by target_artist
+    table_name = str(len(tracks_by_artist)) + " results for " + target_artist
+
+    return render_template('tracks/browse_tracks_by_category.html', headings=header, table_name=table_name, tracks=tracks_by_artist, category=category)
+
+def search_by_date(target_date):
+    header = ["Track Id", "Track Name", "Artist", "Length"]
+    category = "date"
+    # Search for the date.
+    print(target_date)
+
+    try:
+        tracks, prev_date, next_date = services.get_tracks_by_date(int(target_date), repo.repo_instance)
+    except ValueError:
+        return redirect(url_for('search_bp.not_found'))
+
+    # number of tracks found by target_date
+    table_name = str(len(tracks)) + " results for year " + str(target_date)
+
+
+    return render_template('tracks/browse_tracks_by_category.html', headings=header, table_name=table_name, tracks=tracks, category=category)
+
+def search_by_genre(target_genre):
+    header = header = ['Genre Id', 'Title']
+    category = "genre"
+    # Search for the genre.
+
+    tracks_by_genre = services.get_tracks_by_genre(target_genre, repo.repo_instance)
+
+    if len(tracks_by_genre) == 0:
+        return redirect(url_for('search_bp.not_found'))
+
+    # number of tracks found by target_genre
+    table_name = str(len(tracks_by_genre)) + " results for " + target_genre
+
+    return render_template('tracks/browse_tracks_by_category.html', headings=header, table_name=table_name, tracks=tracks_by_genre, category=category)
+
+def search_by_track(target_track):
+    header = ["Track Id", "Track Name", "Artist", "Album"]
+    # Search for the track.
+
+    tracks_by_track = services.get_tracks_by_track(target_track, repo.repo_instance)
+    if len(tracks_by_track) == 0:
+        return redirect(url_for('search_bp.not_found'))
+
+    # number of tracks found by target_track
+    table_name = str(len(tracks_by_track)) + " results for " + target_track
+
+    return render_template('tracks/browse_tracks_by_category.html', headings=header, table_name=table_name, tracks=tracks_by_track)
 # Tracks by Album
-@search_blueprint.route("/search_by_album", methods=['GET', 'POST'])
-def get_album_view():
+@search_blueprint.route("/search_by_album/<album>", methods=['GET', 'POST'])
+def get_album_view(album):
     header = ["Album Id", "Title"]
     try:
         # See if user has put anything in search box and pressed submit
@@ -28,7 +130,7 @@ def get_album_view():
 
 
 # when user types in str
-@search_blueprint.route("/search_by_album/<target_album>", methods=['GET', 'POST'])
+@search_blueprint.route("/search_by_album/album_title", methods=['GET', 'POST'])
 def get_tracks_by_album_str(target_album):
     header = ["Track Id", "Track Name", "Artist", "Album"]
 
@@ -300,3 +402,8 @@ def get_tracks_by_date(target_date):
     return render_template('tracks/browse_tracks.html', headings=header,
                            tracks=tracks, prev_track_url=prev_track_url, next_track_url=next_track_url,
                            first_track_url=first_track_url, last_track_url=last_track_url, table_name=table_name)
+
+class SearchForm(FlaskForm):
+    search = StringField('Search', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    search_type = SelectField('Search Type', choices=[('artist', 'Artist'), ('album', 'Album'), ('genre', 'Genre'), ('date', 'Date'), ('track', 'Track')])
