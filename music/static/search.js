111dae9755d863339b8b995fee501137614bb1cf
Antinson <ant.mitchinson@gmail.com>
2023-10-28 16:59:09 +1300

    Better search

--- music/static/search.js
+++ music/static/search.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const trackCardTemplate = document.querySelector("[data-track-template]");
     const albumCardContainer = document.querySelector("[album-cards-container]");
@@ -8,40 +6,67 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const searchInput = document.querySelector("[data-search]");
 
-    searchInput.addEventListener("input", e => { 
-        const value = e.target.value;
-        tracks.forEach(track => {
-            const isVisible = track.title.includes(value) || track.album.includes(value) || track.artist.includes(value);
-            track.element.classList.toggle("hide", !isVisible);
-        })
-    });
-
-
+    // Define the number of records to display initially
+    const initialRecordCount = 10;
 
     fetch("/getCards")
         .then(response => response.json())
         .then(data => {
-        tracks = data.map(track => {
-            const card = trackCardTemplate.content.cloneNode(true).children[0];
-            const title = card.querySelector("[data-title]");
-            const album = card.querySelector("[data-album]");
-            const artist = card.querySelector("[data-artist]");
-            const trackUrl = card.querySelector("[data-track-url]");
-            const trackDuration = card.querySelector("[data-track-duration]");
-            const genres = card.querySelector("[data-genres]");
-            title.innerText = track.title;
-            album.innerText = track.album.name;
-            artist.innerText = track.artist.full_name;
-            trackUrl.innerText = track.track_url;
-            trackDuration.innerText = track.track_duration;
-            genres.innerText = track.track_genres[0].name;
-            albumCardContainer.appendChild(card);
-            return {title: track.title, album: track.album.name, artist: track.artist.full_name, duration: track.track_duration
-            ,genres: track.track_genres.name, track_url: track.track_url, element: card};
-        })
-    })
+            tracks = data.map(track => {
+                if (track === null) {
+                    return null;
+                }
+                try {
+                    const card = trackCardTemplate.content.cloneNode(true).children[0];
+                    const title = card.querySelector("[data-title]");
+                    const album = card.querySelector("[data-album]");
+                    const artist = card.querySelector("[data-artist]");
+                    const trackUrl = card.querySelector("[data-track-url]");
+                    const trackDuration = card.querySelector("[data-track-duration]");
+                    const genres = card.querySelector("[data-genres]");
+                    title.innerText = track.title;
+                    album.innerText = track.album.name;
+                    artist.innerText = track.artist.full_name;
+                    trackUrl.innerText = track.track_url;
+                    trackDuration.innerText = track.track_duration;
+                    genres.innerText = track.track_genres[0].name;
+
+                    card.addEventListener("click", () => { 
+                        localStorage.setItem("selectedTrackId", track.track_id);
+                        window.location.href = `/track/${track.track_id}`;
+                    });
+
+                    albumCardContainer.appendChild(card);
+                    return {
+                        title: track.title,
+                        album: track.album.name,
+                        artist: track.artist.full_name,
+                        duration: track.track_duration,
+                        genres: track.track_genres.name,
+                        track_url: track.track_url,
+                        element: card,
+                    };
+                } catch (error) {
+                    return null;
+                }
+            })
+            .filter(track => track !== null);
+
+            // Hide records beyond the initial limit
+            tracks.slice(initialRecordCount).forEach(track => {
+                track.element.classList.add("hide");
+            });
+        });
+
+    searchInput.addEventListener("input", e => {
+        const value = e.target.value.toLowerCase();
+        tracks.forEach(track => {
+            const isVisible =
+                value === "" || // Display all records if the search input is empty
+                track.title.toLowerCase().includes(value) ||
+                track.album.toLowerCase().includes(value) ||
+                track.artist.toLowerCase().includes(value);
+            track.element.classList.toggle("hide", !isVisible);
+        });
+    });
 });
-
-
-
-
--- music/tracks/search.py
+++ music/tracks/search.py
@@ -28,7 +28,7 @@ def searchgets():
     try:
         ids = services.get_all_track_ids(repo.repo_instance)
         print(ids)
-        result = services.get_tracks_by_id([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], repo.repo_instance)
+        result = services.get_tracks_by_id(ids, repo.repo_instance)
         print(result)
         return jsonify(result)
     except Exception as e:
