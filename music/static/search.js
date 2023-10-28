f183412699e681019ecf24255029190d81f5a90a
Antinson <ant.mitchinson@gmail.com>
2023-10-28 19:32:10 +1300

    Added loading wheel & fixed search

--- music/static/search.js
+++ music/static/search.js
@@ -1,42 +1,27 @@
+const trackCardTemplate = document.querySelector("[data-track-template]");
+const albumCardContainer = document.querySelector("[album-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+const initialRecordCount = 5;
+let tracks = [];
+let trackElements = [];
+let filteredTracks = [];
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    const trackCardTemplate = document.querySelector("[data-track-template]");
-    const albumCardContainer = document.querySelector("[album-cards-container]");
-
-    let tracks = [];
-
-    const searchInput = document.querySelector("[data-search]");
 
     // Define the number of records to display initially
-    const initialRecordCount = 10;
 
     fetch("/getCards")
         .then(response => response.json())
         .then(data => {
+            let count = -1;
             tracks = data.map(track => {
                 if (track === null) {
                     return null;
                 }
-                try {
-                    const card = trackCardTemplate.content.cloneNode(true).children[0];
-                    const title = card.querySelector("[data-title]");
-                    const album = card.querySelector("[data-album]");
-                    const artist = card.querySelector("[data-artist]");
-                    const trackUrl = card.querySelector("[data-track-url]");
-                    const trackDuration = card.querySelector("[data-track-duration]");
-                    const genres = card.querySelector("[data-genres]");
-                    title.innerText = track.title;
-                    album.innerText = track.album.name;
-                    artist.innerText = track.artist.full_name;
-                    trackUrl.innerText = track.track_url;
-                    trackDuration.innerText = track.track_duration;
-                    genres.innerText = track.track_genres[0].name;
-
-                    card.addEventListener("click", () => { 
-                        localStorage.setItem("selectedTrackId", track.track_id);
-                        window.location.href = `/track/${track.track_id}`;
-                    });
-
-                    albumCardContainer.appendChild(card);
+                try {              
+                    
+                    count++;
                     return {
                         title: track.title,
                         album: track.album.name,
@@ -44,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         duration: track.track_duration,
                         genres: track.track_genres.name,
                         track_url: track.track_url,
-                        element: card,
+                        track_array_index: count
                     };
                 } catch (error) {
                     return null;
@@ -52,21 +37,96 @@ document.addEventListener("DOMContentLoaded", function() {
             })
             .filter(track => track !== null);
 
-            // Hide records beyond the initial limit
-            tracks.slice(initialRecordCount).forEach(track => {
-                track.element.classList.add("hide");
+            tracks.forEach(track => {
+                createTrackElements(track);
             });
+
+            // Display the initial number of records
+            displayTrackElements(tracks.slice(0, initialRecordCount));
         });
 
     searchInput.addEventListener("input", e => {
+
         const value = e.target.value.toLowerCase();
-        tracks.forEach(track => {
-            const isVisible =
-                value === "" || // Display all records if the search input is empty
-                track.title.toLowerCase().includes(value) ||
-                track.album.toLowerCase().includes(value) ||
-                track.artist.toLowerCase().includes(value);
-            track.element.classList.toggle("hide", !isVisible);
-        });
+        albumCardContainer.innerHTML = '';
+
+        filteredTracks = [];
+
+        filteredTracks = tracks.filter(track => {
+            return(
+            track.title.toLowerCase().includes(value) ||
+            track.album.toLowerCase().includes(value) ||
+            track.artist.toLowerCase().includes(value)
+            );
+        })
+        
+        displayTrackElements(filteredTracks.slice(0, initialRecordCount));
     });
 });
+
+
+async function createTrackElements(track) {
+    
+    // Create the elements
+    const card = trackCardTemplate.content.cloneNode(true).children[0];
+    const title = card.querySelector("[data-title]");
+    const album = card.querySelector("[data-album]");
+    const artist = card.querySelector("[data-artist]");
+    const trackUrl = card.querySelector("[data-track-url]");
+    const trackDuration = card.querySelector("[data-track-duration]");
+    //const genres = card.querySelector("[data-genres]");
+
+    // Assign the elements
+    title.innerText = track.title;
+    album.innerText = track.album;
+    artist.innerText = track.artist;
+    trackUrl.innerText = track.track_url;
+    trackDuration.innerText = track.duration;
+    //genres.innerText = track.genres[0];
+
+    // Event listener that goes to the individual track page
+    card.addEventListener("click", () => { 
+        localStorage.setItem("selectedTrackId", track.track_id);
+        window.location.href = `/track/${track.track_id}`;
+    });
+
+    // Push the element to the array
+    trackElements.push(card);
+}
+
+
+async function displayTrackElements(trackArray) {
+    trackArray.forEach(track => {
+        // Appends the element object to the container
+        albumCardContainer.appendChild(trackElements[track.track_array_index]);
+    });
+}
+
+function loadMore() {
+    const currentRecordCount = albumCardContainer.childElementCount;
+    const newRecordCount = currentRecordCount + initialRecordCount;
+    const trackArray = filteredTracks.length ? filteredTracks : tracks;
+    displayTrackElements(trackArray.slice(currentRecordCount, newRecordCount));
+}
+
+window.addEventListener("scroll", () => {
+    const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+        showLoadingIndicator();
+        setTimeout(loadMore, 1000);
+    } else {
+        hideLoadingIndicator();
+    }
+});
+
+function showLoadingIndicator() {
+    const loadingIndicator = document.getElementById("loading-indicator");
+    loadingIndicator.style.display = "block";
+}
+
+function hideLoadingIndicator() {
+    const loadingIndicator = document.getElementById("loading-indicator");
+    loadingIndicator.style.display = "none";
+  }
+  
